1a3f4bbaa6e850419ef5107611b7b5c9fedfff46
Adam Brewer <adamcbrewer@gmail.com>
2012-08-15 17:52:16 +0100

    Static server and http.request set up to fetch changesets

--- app.js
+++ app.js
@@ -1,31 +1,93 @@
 var	https = require('https'),
+	http = require('http'),
 	config = require('./config.js'),
-	
-	// options for our HTTP request object
-	opts = {
-		host: 'api.bitbucket.org',
-		port: 443,
-		method: 'GET',
-		path: '/1.0/repositories/adamcbrewer/tool-miner/changesets?limit=2',
-		auth: config.auth,
-		headers: {
-			'content-type': 'application/json'
+	express = require('express'),
+	io = require('socket.io'),
+
+
+
+	server = express(),
+	socketServer = http.createServer(server),
+	io = io.listen(socketServer),
+
+
+	// Our application settings and configs
+	app = {
+
+		// options for our HTTP request object
+		opts: {
+			host: 'api.bitbucket.org',
+			port: 443,
+			method: 'GET',
+			path: '/1.0/repositories/adamcbrewer/tool-miner/changesets?limit=2',
+			auth: config.auth,
+			headers: {
+				'content-type': 'application/json'
+			}
+		},
+
+		latestResults: null,
+
+		init: function (res) {
+			this.response = res;
+		},
+
+		output: function (json) {
+			this.response.send(json);
+		},
+
+		getChangesets: function (res) {
+			
+			var that = this,
+				changesets = null;
+
+			https.request(this.opts, function(res) {
+
+			// console.log('STATUS: ' + res.statusCode);
+			// console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+				res.setEncoding('utf8');
+				
+				var json = '';
+				res.on('data', function (chunk) {
+						json += chunk;
+					})
+					.on('end', function () {
+						that.latestResults = json;
+						that.output(json);
+					});
+
+			}).end();
+
 		}
+
 	};
 
-https.request(opts, function(res) {
 
-	console.log('STATUS: ' + res.statusCode);
-	console.log('HEADERS: ' + JSON.stringify(res.headers));
 
-	res.setEncoding('utf8');
-	
-	var json = '';
-	res.on('data', function (chunk) {
-			json += chunk;
-		})
-		.on('end', function () {
-			console.log(json);
-		});
 
-}).end();
+
+// This code sets up the HTML server where we want users to visit.
+//
+// When a user lands on this page, a new http request should be fired
+// to retreive the account changeset.
+server.listen(8888);
+
+// Set up our routes
+server.get('/', function (req, res) {
+
+	// TODO: HTML templates
+	app.init(res);
+	app.getChangesets();
+
+});
+
+
+// io.sockets.on('connection', function (socket) {
+// 	console.log('sockets!');
+// 	socket.emit('news', { hello: 'world' });
+// 	socket.on('my other event', function (data) {
+// 		console.log(data);
+// 	});
+// });
+
